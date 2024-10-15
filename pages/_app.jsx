ce27faf60b4e7cbdec28666c7cb3115d2d55a59e
Gvaurdin <gvaurdin777@yandex.ru>
2024-10-15 17:13:32 +0300

    Исправил ошибку импорта Header в файле _app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,16 @@
 import "@/styles/globals.css";
 import { Footer } from '../components/Footer';
-import { Header } from '../components/header';
+import { Header } from '../components/Header';
 
 
 export default function MyApp({ Component, pageProps }) {
   console.log('Component=', Component);
   return <>
-      <Header />
-      <main>
-          <Component {...pageProps} />
-      </main>
-      <Footer />
+    <Header />
+    <main>
+      <Component {...pageProps} />
+    </main>
+    <Footer />
 
   </>
 }
